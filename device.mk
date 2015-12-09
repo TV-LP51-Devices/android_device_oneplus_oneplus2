4434bd72e521dd5e41647569f5a321bea9a6cdbf
Martinusbe <martinusbe@gmail.com>
2015-12-09 17:57:04 +0100

    Revert "oneplus2: Build the BoringSSL compatibility wrapper"
    This reverts commit c4d66835c2285a9fd0529f9f2f0d8a20b96265b5.

--- device.mk
+++ device.mk
@@ -129,10 +129,6 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing
 
-# BoringSSL compatability wrapper
-PRODUCT_PACKAGES += \
-    libboringssl-compat
-
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
